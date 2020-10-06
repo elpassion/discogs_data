a44b0ca8291fd15b780ccf5b15b4a810edba6293
Karol Sarnacki <616132+karolsarnacki@users.noreply.github.com>
2020-10-06 23:29:52 +0200

    Add master_main_release to Release fields

--- lib/discogs_data/model/release.rb
+++ lib/discogs_data/model/release.rb
@@ -1,6 +1,6 @@
 module DiscogsData
   module Model
-    Release = Struct.new(:id, :status, :images, :artists, :title, :labels, :extra_artists, :formats, :genres, :styles, :country, :released, :notes, :data_quality, :master_id, :tracklist, :identifiers, :videos, :companies) do
+    Release = Struct.new(:id, :status, :images, :artists, :title, :labels, :extra_artists, :formats, :genres, :styles, :country, :released, :notes, :data_quality, :master_id, :master_main_release, :tracklist, :identifiers, :videos, :companies) do
       def primary_image
         images&.detect { |image| image.type == "primary" }
       end
